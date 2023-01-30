1539a36e2ecc2569106f4d8197a80fa2d8b793eb
ななしの白狐 <sirokitsune@nanashinobyakkonoMacBook-Pro.local>
2023-01-30 14:35:50 +0900

    トップ画面のルーティングコンポーネントにprops要素を追加

--- src/pages/Top.jsx
+++ src/pages/Top.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../components/Header'
 import Sideber from '../components/Sideber'
 import { Routing } from '../components/Routing'
+import { SideberLinkData } from '../components/SideberData';
 
 function Top() {
   return (
@@ -9,7 +10,7 @@ function Top() {
         <Header />
         <main className="main">
           <Sideber />
-          <Routing />
+          <Routing routeDate={SideberLinkData} />
         </main>
     </>
   )
