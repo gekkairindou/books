63c01cc09a73dbba4b10116e939fd27ebb0ccf5c
ななしの白狐 <sirokitsune@nanashinobyakkonoMacBook-Pro.local>
2023-01-30 14:34:08 +0900

    ルーティングコンポーネントに汎用性を持たせた

--- src/components/Routing.jsx
+++ src/components/Routing.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Route, Routes} from 'react-router-dom';
-import { SideberLinkData } from './SideberData';
 
-export const Routing = () => {
+
+export const Routing = (props) => {
     return (
         <>
             <Routes>
-                {SideberLinkData.map((value, key) => {
+                {props.routeDate.map((value, key) => {
                     return (
                         <Route path={value.link} element={value.component} key={key} />
                     )
